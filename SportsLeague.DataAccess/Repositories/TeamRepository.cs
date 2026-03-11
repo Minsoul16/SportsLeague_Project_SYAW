28ffa18f4ba7dda20c40d57831aafccee8e29458
Minsoul16 <mdiazrestrepo9@gmail.com>
2026-03-11 15:50:25 -0500

    Add project files.

---
+++ SportsLeague.DataAccess/Repositories/TeamRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories;
+
+public class TeamRepository : GenericRepository<Team>, ITeamRepository
+{
+
+    public TeamRepository(LeagueDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Team?> GetByNameAsync(string name)//Devuelve objeto tipo Team
+    {
+        return await _dbSet
+        .FirstOrDefaultAsync(t => t.Name.ToLower() == name.ToLower());
+    }
+
+    public async Task<IEnumerable<Team>> GetByCityAsync(string city)//Devuelve lista de objetos tipo Team
+    {
+        return await _dbSet
+        .Where(t => t.City.ToLower() == city.ToLower())
+        .ToListAsync();
+    }
+
+}
